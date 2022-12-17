9ec13809fec7432d652bd86589eeb14cb2a98b38
9534495797 <chaubeyankit927@gmail.com>
2022-12-17 18:07:03 +0530

    ok

---
+++ forloopsum.py
@@ -0,0 +1,14 @@
+
+# total=0
+# for i in range(1,21):
+#     total+= i
+# print(total)
+# num=(1,21)
+
+
+n=int(input("enter number: "))
+total=0
+for i in range(1,n+1):
+    total+=i
+print(total)
+
