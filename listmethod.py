e20d0d8d859da8e56297fa8ce2a0a3e588047e8d
9534495797 <chaubeyankit927@gmail.com>
2022-12-17 18:22:25 +0530

    ok

---
+++ listmethod.py
@@ -0,0 +1,38 @@
+# x=["a","b"]
+# x.append("c")
+# print(x)
+
+
+# x=["a","b"]
+# x.insert(0,"c")
+# print(x)
+
+
+# x=["a","b"]
+# y=["c","d"]
+# x.append(y)
+# print(x)
+# print(x+y)
+
+# x=["a","b","c","d"]
+# x.pop(3)
+# print(x)
+# #del x[3]
+# print(x)
+# x.remove("c")
+# print(x)
+
+
+
+#count ,sort,reverse,clear,copy method
+
+
+# num=[1,2,3,4,5,4,8,7,0]
+# print(num.count(4))
+# print(sorted(num))
+# num.clear()
+# print(num)
+
+s="string"
+t=s.title()
+print(t)
