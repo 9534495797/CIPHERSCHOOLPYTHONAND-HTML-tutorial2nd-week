408a50c73060ba6385ceb0d48651db8f1f16de0b
9534495797 <chaubeyankit927@gmail.com>
2022-12-17 17:59:33 +0530

    ok

---
+++ dict.py
@@ -0,0 +1,32 @@
+# user={'name':'ankit','age':20}
+# print(user)
+# print(type(user))
+# print(user['name'])
+
+
+
+user_info={
+    'name':'ankit',
+    'age':20,
+    'cast':'brahman',
+    'religious':'hindu',
+    'nation':'hindustan'
+}
+
+# if 'name' in user_info:
+#     print("yes")
+# else:
+#     print("no")
+
+# if 20 in user_info.values():
+#     print("yes")
+# else:
+#     print("no")
+
+# for i in user_info:
+#     print(i)
+
+# for i in user_info.keys():
+#     print(i)
+# for i in user_info.values():
+#     print(i)
