6af631674886e23fd51e213c0f2038f9b191e3a1
9534495797 <chaubeyankit927@gmail.com>
2022-12-17 18:10:13 +0530

    ok

---
+++ function-greater.py
@@ -0,0 +1,25 @@
+# def greater(a,b):
+#     if a>b:
+#         return a
+#     else:
+#         return b
+# print(greater(4,6))
+
+
+
+# def greatest(a,b,c):
+#     if a>b and a>c:
+#         return a
+#     elif b>a and b>c:
+#         return b
+#     else:
+#         return c
+
+
+# def is_palindrom(word):
+#     if word == word[::-1]:
+#         return True
+#     else:
+#         return False
+# print(is_palindrom("naman")) 
+
