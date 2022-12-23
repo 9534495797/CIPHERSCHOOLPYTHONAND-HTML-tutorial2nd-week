ba6cf3e3af8a491f146c875f2d9a97d94a01dc0d
9534495797 <chaubeyankit927@gmail.com>
2022-12-23 21:18:16 +0530

    ok

---
+++ tuplesintro.py
@@ -0,0 +1,16 @@
+# tuples store any data type
+# write in small bracket
+# no append, no insert, no pop ,no reverse
+# faster than list
+# count ,index,slice and len functionn work inn this
+
+# nums=(1,)
+# x=(1)
+# y=('boy',)
+# print(type(nums))
+# print(type(x))
+# print(type(y))
+
+
+x='boy','girl','none'
+print(type(x))
